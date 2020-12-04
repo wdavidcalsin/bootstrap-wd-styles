55ff1233e7abb0ac65a1a3d7ee6943cb5e61d626
Wdavid Calsin <david2c142@gmail.com>
2020-12-04 00:10:50 -0500

    refactor: 💡 added sidebar to select components

---
+++ src/components/nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Nav = () => {
+   return (
+      <div className="sidebar">
+         <ul>
+            <li>Button</li>
+            <li>Badge</li>
+            <li>Alert</li>
+         </ul>
+      </div>
+   );
+};
